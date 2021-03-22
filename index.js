ab8afbd8da53d69f291aee8e95ee46a62608d812
Nworie Kingsley <obizzkingsley@gmail.com>
2021-03-22 06:04:26 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function openMobileNav() {
+  var element = document.getElementById('mobileNav');
+  element.classList.add('appear');
+}
+
+function closeMobileNav() {
+  var element = document.getElementById('mobileNav');
+  element.classList.remove('appear');
+}
