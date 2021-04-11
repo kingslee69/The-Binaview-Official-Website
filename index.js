8b1fca0358b3f2c17d3e1e975c3d110d2bfe2d28
Nworie Kingsley <obizzkingsley@gmail.com>
2021-04-11 18:46:19 +0200

    fully optimized with animations

--- index.js
+++ index.js
@@ -7,3 +7,68 @@ function closeMobileNav() {
   var element = document.getElementById('mobileNav');
   element.classList.remove('appear');
 }
+
+let timer = 0;
+
+window.addEventListener('load', event => {
+  let intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(function () {
+          entry.target.classList.add('up');
+        }, timer);
+        timer += 50;
+        intersectionObserver.unobserve(entry.target);
+        setTimeout(function () {
+          timer = 0;
+        }, 1000);
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate').forEach(obj => {
+    intersectionObserver.observe(obj);
+  });
+});
+
+window.addEventListener('load', event => {
+  let intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(function () {
+          entry.target.classList.add('up');
+        }, timer);
+        timer += 50;
+        intersectionObserver.unobserve(entry.target);
+        setTimeout(function () {
+          timer = 0;
+        }, 1000);
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate-right').forEach(obj => {
+    intersectionObserver.observe(obj);
+  });
+});
+
+window.addEventListener('load', event => {
+  let intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        setTimeout(function () {
+          entry.target.classList.add('up');
+        }, timer);
+        timer += 50;
+        intersectionObserver.unobserve(entry.target);
+        setTimeout(function () {
+          timer = 0;
+        }, 1000);
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate-left').forEach(obj => {
+    intersectionObserver.observe(obj);
+  });
+});
